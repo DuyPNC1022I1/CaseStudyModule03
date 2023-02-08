589c022a9934fbad0743ce1573aaeedb3f66563a
NongDucManh <manhduc.nong93@gmail.com>
2023-02-08 16:05:03 +0700

    update adminservlet

--- src/main/java/controller/AdminServlet.java
+++ src/main/java/controller/AdminServlet.java
@@ -1,7 +1,6 @@
 package controller;
 
-import service.dao.ProductService;
-
+import service.dao.ProductDAO;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,19 +8,14 @@ import java.io.IOException;
 
 @WebServlet(name = "AdminServlet", value = "/admin")
 public class AdminServlet extends HttpServlet {
-    private ProductService productService;
+    private ProductDAO productService;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
         if (action == null) {
             action = "";
         }
-        switch (action) {
-            case "":
-                break;
-            default:
-                break;
-        }
+
     }
 
     @Override
