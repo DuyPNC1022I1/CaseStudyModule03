9b8cff98c0f61fbd915d5e28235b1639c374a4f9
cuongtn98 <cuonghoa1998@gmail.com>
2023-02-09 16:53:12 +0700

    update lỗi

--- src/main/java/controller/LoginControlServlet.java
+++ src/main/java/controller/LoginControlServlet.java
@@ -1,33 +1,33 @@
-package controller;
-
-import dao.Account;
-import dao.LoginDAO;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import java.io.IOException;
-
-@WebServlet(name = "LoginControlServlet", value = "/LoginControlServlet")
-public class LoginControlServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.setContentType();
-    try {
-        String user = request.getParameter("username");
-        String pass = request.getParameter("password");
-        LoginDAO loginDAO = new LoginDAO();
-        Account account = loginDAO.checkLogin(user, pass);
-        if (account == null){
-            response.sendRedirect("Login.jsp");
-        }else {
-            response.sendRedirect("Success.jsp");
-        }
-    } catch (Exception e) {
-    }
-    }
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-}
+//package controller;
+//
+//import dao.Account;
+//import dao.LoginDAO;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.*;
+//import javax.servlet.annotation.*;
+//import java.io.IOException;
+//
+//@WebServlet(name = "LoginControlServlet", value = "/LoginControlServlet")
+//public class LoginControlServlet extends HttpServlet {
+////    @Override
+////    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+////    response.setContentType();
+////    try {
+////        String user = request.getParameter("username");
+////        String pass = request.getParameter("password");
+////        LoginDAO loginDAO = new LoginDAO();
+////        Account account = loginDAO.checkLogin(user, pass);
+////        if (account == null){
+////            response.sendRedirect("Login.jsp");
+////        }else {
+////            response.sendRedirect("Success.jsp");
+////        }
+////    } catch (Exception e) {
+////    }
+////    }
+////    @Override
+////    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+////
+////    }
+////}
