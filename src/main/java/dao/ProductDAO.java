4b46b8b849bfa90c94ac52aa1abf1f6aeb5e2093
NongDucManh <manhduc.nong93@gmail.com>
2023-02-13 10:07:36 +0700

    update database

--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -12,7 +12,7 @@ public class ProductDAO implements Crud<Product> {
     private BrandDAO brandDAO;
     private String jdbcUrl = "jdbc:mysql://localhost:3306/case_module03";
     private String jdbcUsername = "root";
-    private String jdbcPassword = "12345678";
+    private String jdbcPassword = "123456";
 
     public ProductDAO() {
         this.brandDAO = new BrandDAO();
